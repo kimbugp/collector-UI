3002dcc9a4a3281a8a75a9b1e05ae03ce153893a
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-05-27 10:23:17 +0300

    ft(house): add crud
    - add create and delete features
    - add pagination

--- src/others/table.js
+++ src/others/table.js
@@ -1,53 +1,46 @@
 import React from 'react';
 import MaterialTable from 'material-table';
-import HousesAction from '../actions/houses';
+import HousesAction, { createHouse, deleteHouse } from '../actions/houses';
 
 export default function Table(props) {
 
   const [state, setState] = React.useState({
     columns: [
       { title: 'House', field: 'house_name', type: 'string' },
-      { title: 'Rate', field: 'rate', type: 'currency' },
-      // { title: 'Tenant', field: 'tenant', type: 'string' },
+      { title: 'Rate', field: 'rate', type: 'numeric' },
+      { title: 'Tenant', field: 'tenant.email', type: 'string' },
       { title: 'Paid', field: 'is_paid', type: 'boolean' },
       { title: 'Start Date', field: 'start_date', type: 'date' },
     ],
     data: [
-      {
-        identifier: "05848e3a-4ca1-4ec4-bdf1-98f5780062d5",
-        house_name: "Lagos 24th Street",
-        rate: 200000,
-        is_occupied: true,
-        start_date: "2019-04-29",
-        tenant: "kimbugwe.simon-pete@andela.com",
-        is_paid: true,
-        owner: { "email": "simon@simon.com" },
-      },
-      {
-        identifier: "05848e3a-4ca1-4ec4-bdf1-98f5780062d5",
-        house_name: "Lagos 24th Street",
-        rate: 200000,
-        is_occupied: true,
-        start_date: "2019-04-29",
-        tenant: "kimbugwe.simon-pete@andela.com",
-        is_paid: false,
-        owner: { "email": "simon@simon.com" },
-      },
     ],
   });
   return (
     <MaterialTable
       title={props.title}
       columns={state.columns}
-      data={async()=>{
-        let res = await HousesAction()
-        return res
-      }}
+      data={query =>
+        new Promise(async (resolve, reject) => {
+            let url = `page=${query.page + 1}`
+            let res = await HousesAction(url)
+            resolve({
+                data: res.results,
+                page: res.current-1,
+                totalCount: res.count
+            });
+        })
+    }
       editable={{
         onRowAdd: newData =>
           new Promise(resolve => {
-            setTimeout(() => {
+            setTimeout(async () => {
               resolve();
+              let date = newData.start_date.getFullYear()
+                + "-" + (newData.start_date.getMonth() + 1)
+                + "-" + newData.start_date.getDate()
+              let house = newData
+              house.start_date = date
+              await createHouse(house)
               const data = [...state.data];
               data.push(newData);
               setState({ ...state, data });
@@ -64,9 +57,10 @@ export default function Table(props) {
           }),
         onRowDelete: oldData =>
           new Promise(resolve => {
-            setTimeout(() => {
+            setTimeout(async () => {
               resolve();
               const data = [...state.data];
+              await deleteHouse(oldData.identifier)
               data.splice(data.indexOf(oldData), 1);
               setState({ ...state, data });
             }, 600);
